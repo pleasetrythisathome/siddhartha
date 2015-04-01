17f2d26d5bcd0205c99fe87fcd8b01187fe56396
Dylan Butman <dbutman@gmail.com>
2015-04-01 18:51:48 -0400

    async protocol

--- src/siddhartha/core.clj
+++ src/siddhartha/core.clj
@@ -2,12 +2,24 @@
   (:require [clojure.core.async :as async]
             [clojure.set :as set]))
 
-(defn throw-err [e]
-  (when (instance? Throwable e) (throw e))
+(defn throw-err
+  [e]
+  (when (instance? Throwable e)
+    (throw e))
   e)
 
-(defn <? [ch]
+(defn <?
+  "go blocking take with error handling"
+  [ch]
   (throw-err (async/<! ch)))
 
-(defn <?? [ch]
+(defn <??
+  "blocking take with error handling"
+  [ch]
   (throw-err (async/<!! ch)))
+
+(defprotocol IAsyncProtocol
+  (send-chan [_])
+  (receive-chan [_])
+  (sent-events [_])
+  (received-events [_]))

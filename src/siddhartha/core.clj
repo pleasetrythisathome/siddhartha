25d7048344deeee540be89d054aba5f9f24be76d
Dylan Butman <dbutman@gmail.com>
2015-04-01 18:51:30 -0400

    async error handling

--- src/siddhartha/core.clj
+++ src/siddhartha/core.clj
@@ -1,3 +1,13 @@
 (ns siddhartha.core
   (:require [clojure.core.async :as async]
             [clojure.set :as set]))
+
+(defn throw-err [e]
+  (when (instance? Throwable e) (throw e))
+  e)
+
+(defn <? [ch]
+  (throw-err (async/<! ch)))
+
+(defn <?? [ch]
+  (throw-err (async/<!! ch)))
